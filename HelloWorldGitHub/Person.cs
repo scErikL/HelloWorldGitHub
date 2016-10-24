04fe6639cfa63b3487dab02950339e85b78c49e6
Erik Lövbom <erik.lovbom@studentconsulting.com>
2016-10-24 10:02:00 +0200

    new class Person

---
+++ HelloWorldGitHub/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGitHub
+{
+    class Person
+    {
+
+        //Belongs to feature1.1
+    }
+}
